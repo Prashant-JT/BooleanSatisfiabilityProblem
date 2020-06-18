2da8bb7b0ac30788484d80a342f42b89bf428341
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-06-18 20:34:32 +0100

    final version

--- GUI_SAT.py
+++ GUI_SAT.py
@@ -1,21 +1,21 @@
 from Functions.MultiSAT import *
 
 
-def install(package):
+def install_package(package):
     pip.main(['install', package])
 
-
-try:
+# Intenta instalar tkinter y numpy
+try: 
     from tkinter import *
 except ImportError:
     print('tkinter is not installed, installing it now!')
-    install('tkinter')
+    install_package('tkinter')
 
 try:
     import numpy
 except ImportError:
     print('numpy is not installed, installing it now!')
-    install('numpy')
+    install_package('numpy')
 
 
 class GUI:
@@ -28,20 +28,21 @@ class GUI:
         self.algorithm.set(5)  # por defecto CDCL
 
         self.master = master
-        self.master.title("Problema de satisfacibilidad booleana")  # titulo de la ventana
+        self.master.title("Problema de satisfacibilidad booleana") 
         self.master.resizable(0, 0)  # evita redimensionar
         self.master.geometry("650x350")  # tamaño del master
 
-        self.label1 = Label(master, text="Problema de satisfacibilidad booleana", font='Helvetica 10 bold')  # texto
+        self.label1 = Label(master, text="Problema de satisfacibilidad booleana", font='Helvetica 10 bold')  
         self.label1.place(x=200, y=20)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
 
         self.entry = Entry(master, textvariable=self.formula, width="50")  # textbox y le asigna una variable formula
-        self.entry.place(x=52, y=75)  # posicion en el master
+        self.entry.place(x=52, y=75)
 
-        self.button_compute = Button(master, text="Calcula", command=self.compute)  # cuando se pulse el boton, llama al metodo compute
-        self.button_compute.place(x=370, y=72)  # posicion en el master
+    
+        self.button_compute = Button(master, text="Calcula", command=self.compute)  # cuando se pulse el boton, llama al metodo show
+        self.button_compute.place(x=370, y=72)
 
-        self.label1 = Label(self.master, text="Introduzca una fórmula booleana. Formato: (a+!b+!c),(c+d+!a),(!d+b+a)")  # texto
+        self.label1 = Label(self.master, text="Introduzca una fórmula booleana. Formato: (a+!b+!c),(c+d+!a),(!d+b+a)")
         self.label1.place(x=52, y=50)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
         self.formula_p = Radiobutton(master, text="Fórmula personalizada", variable=self.clause_format, value=1, command=self.set_label)  # esta asociada a la variable format_clause
         self.formula_p.place(x=50, y=100)
@@ -49,8 +50,8 @@ class GUI:
         self.files = Radiobutton(master, text="Directorio | Fichero", variable=self.clause_format, value=2, command=self.set_label)  # esta asociada a la variable format_clause
         self.files.place(x=250, y=100)
 
-        self.label2 = Label(master, text="Elija el algoritmo que desea ejecutar")  # texto
-        self.label2.place(x=50, y=140)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+        self.label2 = Label(master, text="Elija el algoritmo que desea ejecutar") 
+        self.label2.place(x=50, y=140) 
 
         self.brute_force = Radiobutton(master, text="Fuerza bruta", variable=self.algorithm, value=1)  # esta asociada a la variable algorithm
         self.brute_force.place(x=50, y=160)
@@ -71,17 +72,16 @@ class GUI:
         self.label3.place(x=50, y=275)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
 
         self.label4 = Label(master, text="No disponible")  # texto
-        self.label4.place(x=50, y=295)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+        self.label4.place(x=50, y=295) 
+
 
     def set_label(self):
         if self.clause_format.get() == 1:
             self.label1.config(text="Introduzca una fórmula booleana. Formato: (a+!b+!c)(c+d+!a)(!d+b+a)")
-            self.label1.place(x=52,
-                              y=50)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+            self.label1.place(x=52, y=50)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
         else:
             self.label1.config(text="Introduzca la ruta relativa del  directorio o fichero")
-            self.label1.place(x=52,
-                              y=50)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+            self.label1.place(x=52, y=50)
 
     def get_clause(self):
         return self.formula.get()
@@ -95,18 +95,17 @@ class GUI:
     def compute(self):
         sat, time, result = main(self.get_clause_format(), self.get_clause(), self.get_algorithm())
         self.show_result(sat, time, result)
-
+    
     def show_result(self, sat, time, result):
-        if time is None:
-            return
+        if time is None: return
         if isinstance(result, dict):
             # unico fichero sat o formula personalizada
             if sat:
-                label = Label(self.master, text="La fórmula introducida es satisfacible")  # texto
+                label = Label(self.master, text="La fórmula introducida es satisfacible") 
                 label.place(x=390, y=295)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
-                scroll = Scrollbar(self.master)
+                scroll = Scrollbar(self.master) # scroll del textarea
                 scroll.place(x=600, y=180)
-                textbox = Text(self.master, width=25, height=10, yscrollcommand=scroll.set)
+                textbox = Text(self.master, width=25, height=10, yscrollcommand=scroll.set) # texarea donde se mostrará las asignaciones
                 textbox.place(x=390, y=120)
                 scroll.config(command=textbox.yview)  # enlaza scrollbar al textbox
                 for key in result.keys():
@@ -114,19 +113,29 @@ class GUI:
                     textbox.insert(END, "\n")
                 textbox.configure(state='disable')  # dehabilita la edicion
             else:
-                label = Label(self.master, text="La fórmula introducida no es satisfacible")  # texto
-                label.place(x=390, y=295)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+                label = Label(self.master, text="La fórmula introducida no es satisfacible") 
+                label.place(x=390, y=295)
+                textbox = Text(self.master, width=25, height=10) # texarea donde se mostrará las asignaciones
+                textbox.place(x=390, y=120)
+                textbox.delete(1.0, END)
+
         else:
             if sat == False:
                 # unico fichero y unsat
-                label = Label(self.master, text="La formula introducida no es satisfacible")  # texto
+                label = Label(self.master, text="La fórmula introducida no es satisfacible") 
                 label.place(x=390, y=295)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+                textbox = Text(self.master, width=25, height=10) # texarea donde se mostrará las asignaciones
+                textbox.place(x=390, y=120)
+                textbox.delete(1.0, END)
             else:
                 # directorio
-                label = Label(self.master, text="El resultado se encuentra en el fichero")  # texto
-                label.place(x=390, y=295)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
-                label1 = Label(self.master, text=result)  # texto
-                label1.place(x=390, y=310)  # posicion en el master (pixeles respecto al borde izq, pixeles respecto al borde superior)
+                label = Label(self.master, text="El resultado se encuentra en el fichero")
+                label.place(x=390, y=295)  
+                label1 = Label(self.master, text=result)
+                label1.place(x=390, y=310) 
+                textbox = Text(self.master, width=25, height=10) # texarea donde se mostrará las asignaciones
+                textbox.place(x=390, y=120)
+                textbox.delete(1.0, END)
         self.label4.config(text=str(time) + " segundos")  # actualiza el tiempo de ejecucion
 
 
